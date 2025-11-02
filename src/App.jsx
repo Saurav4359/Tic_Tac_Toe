d20637449baac8c8da648f79343520845b57271b
Saurav4359 <isauravsharmaokay4359@gmail.com>
2025-11-02 16:02:29 +0530

    board done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
+const Square = ({value,onSquareClick}) => {
 
-const Square = ({player})=>{
-  return(
-    <button className="sqaure">{player}</button>
+   
+  return <button className="sqaure" onClick={onSquareClick}>{value}</button>;
+};
+
+function App() {
+  const [square ,setsquare]=useState(Array(9).fill(null));
+  const handleClick=(i)=>{
+     const nextSq=square.slice();
+     nextSq[i]='X';
+     setsquare(nextSq);
+  }
+  return (
+    <>
+      <div className="row">
+        <Square value={square[0]} onSquareClick={()=>handleClick(0)}/>
+        <Square value={square[1]} onSquareClick={()=>handleClick(1)}/>
+        <Square value={square[2]} onSquareClick={()=>handleClick(2)}/>
+      </div>
+      <div className="row">
+        <Square value={square[3]} onSquareClick={()=>handleClick(3)}/>
+        <Square value={square[4]} onSquareClick={()=>handleClick(4)}/>
+        <Square value={square[5]} onSquareClick={()=>handleClick(5)}/>
+      </div>
+      <div className="row">
+        <Square value={square[6]} onSquareClick={()=>handleClick(6)}/>
+        <Square value={square[7]} onSquareClick={()=>handleClick(7)}/>
+        <Square value={square[8]} onSquareClick={()=>handleClick(8)}/>
+      </div>
+    </>
   );
 }
-function App() {
-  return(<>
-    <div className="row">
-  <Square player={'X'}/>
-  <Square player={'X'}/>
-  <Square player={'X'}/>
-    </div>
-      <div className="row">
-  <Square player={'X'}/>
-  <Square player={'X'}/>
-  <Square player={'X'}/>
-    </div>
-      <div className="row">
-  <Square player={'X'}/>
-  <Square player={'X'}/>
-  <Square player={'X'}/>
-    </div>
 
-
-     
-  </>);
-}
-
-export default App
+export default App;
